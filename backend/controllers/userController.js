cbbce51e03bdb80b405000fd1dcbc9a2e05e78e0
Clay <claymcintosh@gmail.com>
2022-05-14 09:05:30 +1000

    Login working (in insomnia)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,7 +37,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id),
         })
     } else {
         res.status(400)
@@ -49,7 +50,22 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: 'Login User' })
+    const { email, password } = req.body
+
+    //check for user email
+    const user = await User.findOne({ email })
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
 })
 
 // @desc Get user data
@@ -58,6 +74,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
     res.json({ message: 'User data display' })
 })
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 
 module.exports = {
