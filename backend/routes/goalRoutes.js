fa9798ea24ca23921d784800aec672035e67efa8
Clay <claymcintosh@gmail.com>
2022-05-13 15:36:05 +1000

    Simple rest api set up. Not doing anything yet.

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.status(200).json({ message: 'Get Goals' })
+})
+
+router.post('/', (req, res) => {
+    res.status(200).json({ message: 'set Goals' })
+})
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({ message: `update goal. ${req.params.id}` })
+})
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({ message: `delete goal. ${req.params.id}` })
+})
+
+
+
+module.exports = router
