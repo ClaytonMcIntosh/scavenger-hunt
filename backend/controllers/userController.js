a83200da7fafa31d0458ee2a4f36002dafdd7826
Clay <claymcintosh@gmail.com>
2022-05-14 08:37:20 +1000

    We can now register a user (with insomnia)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,12 +8,41 @@ const User = require('../models/userModel')
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
-    
-    if(!name || !email || !password) {
+
+    if (!name || !email || !password) {
         res.status(400)
         throw new Error('Please add all fields')
     }
-    res.json({ message: 'Register User' })
+
+    //check if user exists
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 // @desc Authenticate a user
