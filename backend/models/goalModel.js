408b26c5a7ebbb16ccd3138ba90a0b95d169a1e9
Clay <claymcintosh@gmail.com>
2022-05-14 07:11:13 +1000

    Added userModel

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose')
 
-const goalSchema = mongoose.Schema({
-    text: {
-        type: String,
-        required: [true, 'Please add a text value']
-    }
-}, {
+const goalSchema = mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        text: {
+            type: String,
+            required: [true, 'Please add a text value']
+        }
+    }, {
     timestamps: true
 })
 
---
+++ backend/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        require: [true, 'Please add an email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        require: [true, 'Please add a password']
+    },
+})
+{
+    timestams: true
+})
+
+module.exports = mongoose.model('User', userSchema)
