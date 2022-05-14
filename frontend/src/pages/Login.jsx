c8f619b5245e8badc940ba1f7792d9f518e71e28
Clay <claymcintosh@gmail.com>
2022-05-14 15:54:21 +1000

    Added front end UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,46 @@
+import { useState, useEffect } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+
 function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const { email, password } = formData
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <section className='heading'>
+                <h1> <FaSignInAlt />Login </h1>
+                <p> this is the login page</p>
+            </section>
+
+            <section className="form">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input type='email' className='form-control' id='email' name='email' value={email} placeholder='Enter your email' onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <input type='password' className='form-control' id='password' name='password' value={password} placeholder='Enter password' onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className='btn btn-block'>Submit</button>
+                    </div>
+                </form>
+            </section>
+        </>
     )
 }
 
