a62c7e56a20eb5b6ca18740d40e2a5e0b9acf6bd
Clay <claymcintosh@gmail.com>
2022-05-15 09:58:21 +1000

    login working again

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+
 function Dashboard() {
+    const navigate = useNavigate()
+
+    const { user } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login')
+        }
+    }, [user, navigate])
+
     return (
         <div>
             Dashboard
