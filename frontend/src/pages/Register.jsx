a1896bd961db3307062bb614b3937f995b60feb6
Clay <claymcintosh@gmail.com>
2022-05-15 06:35:33 +1000

    Register now loading

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -19,7 +19,9 @@ function Register() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
-    const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
+    const { user, isLoading, isError, isSuccess, message } = useSelector(
+        (state) => state.auth
+    )
 
     useEffect(() => {
         if (isError) {
@@ -31,9 +33,7 @@ function Register() {
         }
 
         dispatch(reset())
-
     }, [user, isError, isSuccess, message, navigate, dispatch])
-
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -56,39 +56,76 @@ function Register() {
 
             dispatch(register(userData))
         }
-
-        if (isLoading) {
-            return <Spinner />
-        }
-
-        return (
-            <>
-                <section className='heading'>
-                    <h1> <FaUser />Register </h1>
-                    <p> Please create an account</p>
-                </section>
-
-                <section className="form">
-                    <form onSubmit={onSubmit}>
-                        <div className="form-group">
-                            <input type='text' className='form-control' id='name' name='name' value={name} placeholder='Enter your name' onChange={onChange} />
-                        </div>
-                        <div className="form-group">
-                            <input type='email' className='form-control' id='email' name='email' value={email} placeholder='Enter your email' onChange={onChange} />
-                        </div>
-                        <div className="form-group">
-                            <input type='password' className='form-control' id='password' name='password' value={password} placeholder='Enter password' onChange={onChange} />
-                        </div>
-                        <div className="form-group">
-                            <input type='password' className='form-control' id='password2' name='password2' value={password2} placeholder='Confirm password' onChange={onChange} />
-                        </div>
-                        <div className="form-group">
-                            <button type="submit" className='btn btn-block'>Submit</button>
-                        </div>
-                    </form>
-                </section>
-            </>
-        )
     }
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
+    return (
+        <>
+            <section className='heading'>
+                <h1>
+                    <FaUser /> Register
+                </h1>
+                <p>Please create an account</p>
+            </section>
+
+            <section className='form'>
+                <form onSubmit={onSubmit}>
+                    <div className='form-group'>
+                        <input
+                            type='text'
+                            className='form-control'
+                            id='name'
+                            name='name'
+                            value={name}
+                            placeholder='Enter your name'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            type='email'
+                            className='form-control'
+                            id='email'
+                            name='email'
+                            value={email}
+                            placeholder='Enter your email'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            type='password'
+                            className='form-control'
+                            id='password'
+                            name='password'
+                            value={password}
+                            placeholder='Enter password'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            type='password'
+                            className='form-control'
+                            id='password2'
+                            name='password2'
+                            value={password2}
+                            placeholder='Confirm password'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <button type='submit' className='btn btn-block'>
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
+    )
 }
+
 export default Register
