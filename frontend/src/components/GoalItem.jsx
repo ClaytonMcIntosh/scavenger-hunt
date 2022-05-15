27395f0d809842ff8cf8dc30a546f9d12b3e6773
Clay <claymcintosh@gmail.com>
2022-05-15 11:53:00 +1000

    Boiler Plate compleate

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -10,7 +10,7 @@ function GoalItem({ goal }) {
             <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
             <h2>{goal.text}</h2>
             <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
-                X
+                x
             </button>
         </div>
     )
