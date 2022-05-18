b4d67edf93f5c57447cd093d1e349735165ea14f
Clay <claymcintosh@gmail.com>
2022-05-18 13:16:14 +1000

    Playing with inputs and state

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Card = ({ img, heading, found }) => {
+
+    const [message, setMessage] = useState('');
+
+    const handleChange = event => {
+        setMessage(event.target.value);
+    };
+
+    const handleClick = event => {
+        event.preventDefault();
+
+        // 👇️ value of input field
+        console.log('old value: ', message);
+
+        // 👇️ set value of input field
+        setMessage('New value');
+    };
+
+
+
     return (
         <div className='square'>
-            <img className="card-image" src={img} alt="" width="100%" />
+            <img className="card-image" src={img} alt="" width="20%" />
             <h3>{heading} </h3>
-            {found ? <div>found</div> : ''}
-            {!found ? <div>not found</div> : ''}
             <div className='found-box'>
-                <div className='found-text'> not yet found
-                </div>
-                <div className='found-check'> x
-                </div>
+                {found ? <>
+                    <div className='found-check'> x </div>
+                    <div>found</div>
+                </>
+                    : ''}
+                {!found ? <>
+                    <div>not found</div>
+
+
+                    <div>
+                        <input
+                            type="text"
+                            id="message"
+                            name="message"
+                            onChange={handleChange}
+                            value={message}
+                        />
+
+                        <h2>Message: {message}</h2>
+
+                        <button onClick={handleClick}>Click</button>
+                    </div>
+
+
+                </>
+                    : ''}
             </div>
         </div >
     )
