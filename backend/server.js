2df1778c676c239951cf0e890d194deff934ecda
Clay <claymcintosh@gmail.com>
2022-05-20 16:10:57 +1000

    Deploy again

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const path = require('path')
+// const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -16,11 +16,15 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/hunts', require('./routes/huntRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-//Serve front end
+// Serve frontend
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    )
 } else {
     app.get('/', (req, res) => res.send('Please set to production'))
 }
