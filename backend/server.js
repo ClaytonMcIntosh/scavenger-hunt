db431aeecebf9f6fc37f36e6435ace8273230e91
Clay <claymcintosh@gmail.com>
2022-05-20 15:00:27 +1000

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -14,6 +15,15 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/hunts', require('./routes/huntRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+//Serve front end
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler)
 
